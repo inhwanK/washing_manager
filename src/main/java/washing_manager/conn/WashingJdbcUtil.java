be0d73b9a0fcd2575deecc5d49e0e8efeda4ed36
inhwanK <inhan1009@naver.com>
2021-03-10 21:11:41 +0900

    WashingJdbcUtil 작성 중. properties 파일이 있는 이유를 알았고 위 파일을 바탕으로 properties 객체를 생성해서 이용한다는 점까지 알았음.

---
+++ src/main/java/washing_manager/conn/WashingJdbcUtil.java
@@ -0,0 +1,22 @@
+package washing_manager.conn;
+
+import java.io.InputStream;
+import java.sql.Connection;
+
+public class WashingJdbcUtil {
+	
+	public Connection getConnection() {
+		String propsPath = "db.properties";
+		
+		//작성 중
+		try(InputStream is = con){
+			
+		}catch() {
+			
+		}
+		
+		return null;
+		
+	}
+
+}
